b6e0ac746da00b56b824a5e4ded941a11d5dc4a5
André Roque Matheus <amatheus@mac.com>
2021-12-01 15:06:53 -0300

    solve first day problem (part 1 and 2)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun List<Int>.countIncreases() =
+        this.zipWithNext()
+            .map { it.second > it.first }
+            .count { it }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map(String::toInt)
+            .countIncreases()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map(String::toInt)
+            .windowed(3)
+            .map { it.sum() }
+            .countIncreases()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
